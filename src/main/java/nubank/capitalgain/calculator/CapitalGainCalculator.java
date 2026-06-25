0e4ff9dc13951f519313130c534962a208b8a1d7
Lívia Silva Santos <liviasilvasantos@gmail.com>
2026-06-25 18:30:08 -0300

    add ticker to operation

--- src/main/java/nubank/capitalgain/calculator/CapitalGainCalculator.java
+++ src/main/java/nubank/capitalgain/calculator/CapitalGainCalculator.java
@@ -5,16 +5,21 @@ import nubank.capitalgain.domain.OperationType;
 import nubank.capitalgain.domain.Tax;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CapitalGainCalculator {
 
-    private ProcessOperation processOperation = new ProcessOperation();
+    private Map<String, ProcessOperation> operationMap= new HashMap<>();
 
     public List<Tax> processAll(List<Operation> operations) {
         var taxes = new ArrayList<Tax>();
 
         operations.forEach((operation) -> {
+
+            operationMap.computeIfAbsent(operation.ticker(), k -> new ProcessOperation());
+
             var tax = process(operation);
             taxes.add(tax);
         });
@@ -23,13 +28,21 @@ public class CapitalGainCalculator {
     }
 
     private Tax process(Operation operation) {
+
+        var processOperation = operationMap.get(operation.ticker());
+
         if (OperationType.buy.equals(operation.operation()))
             return processOperation.buy(operation);
 
         return processOperation.sell(operation);
     }
 
-    public ProcessOperation getProcessOperation() {
-        return processOperation;
+    public ProcessOperation getProcessOperation(){
+        operationMap.computeIfAbsent(Operation.DEFAULT_TICKER, k -> new ProcessOperation());
+        return operationMap.get(Operation.DEFAULT_TICKER);
+    }
+
+    public ProcessOperation getProcessOperation(String ticker) {
+        return operationMap.get(ticker);
     }
 }
--- src/main/java/nubank/capitalgain/domain/Operation.java
+++ src/main/java/nubank/capitalgain/domain/Operation.java
@@ -5,7 +5,14 @@ import java.math.BigDecimal;
 import static java.math.BigDecimal.valueOf;
 
 public record Operation(OperationType operation, @JsonProperty("unit-cost") BigDecimal unitCost,
-                        Integer quantity) {
+                        Integer quantity, String ticker) {
+
+    public final static String DEFAULT_TICKER =  "DEFAULT_TICKER";
+
+    public Operation(OperationType operation, BigDecimal unitCost,
+                     Integer quantity) {
+        this(operation, unitCost, quantity, DEFAULT_TICKER); // Define 18 como o valor padrão para idade
+    }
 
     public BigDecimal value() {
         return unitCost.multiply(valueOf(quantity));
