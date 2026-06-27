78f9d89607082bbbc66c33529eb3d410e9ec032f
Lívia Silva Santos <liviasilvasantos@gmail.com>
2026-06-27 17:25:49 -0300

    add input and output files

--- src/main/java/nubank/capitalgain/Main.java
+++ src/main/java/nubank/capitalgain/Main.java
@@ -5,30 +5,36 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import nubank.capitalgain.calculator.CapitalGainCalculator;
 import nubank.capitalgain.domain.Operation;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class Main {
     private static final ObjectMapper mapper = new ObjectMapper();
 
     public static void main(String[] args) {
+        File outputFile = new File("output.txt");
         var calculator = new CapitalGainCalculator();
 
-        try (var reader = new BufferedReader(new InputStreamReader(System.in))) {
+        try (var reader = new BufferedReader(new InputStreamReader(System.in));
+             var writer = new PrintWriter(new BufferedWriter(new FileWriter(outputFile, StandardCharsets.UTF_8)))) {
+
             String currentLine;
 
             while ((currentLine = reader.readLine()) != null) {
                 if (currentLine.trim().isEmpty()) continue;
 
+                System.out.println("input = %s".formatted(currentLine));
                 var operations = mapper.readValue(currentLine, new TypeReference<List<Operation>>() {
                 });
 
                 var taxes = calculator.processAll(operations);
 
-                System.out.println(mapper.writeValueAsString(taxes));
+                System.out.println("output = %s".formatted(mapper.writeValueAsString(taxes)));
+                writer.println(mapper.writeValueAsString(taxes));
             }
+
+            System.out.println("Processamento concluído. Verifique o arquivo: " + outputFile.getAbsolutePath());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
